39b917aeaa7b99b594a8b3c2ccedddd168a812c5
Ben Swanson <bswan0002@gmail.com>
2020-10-10 03:22:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 def meal_choice (arg1, arg2 = "meat")
   puts "What a nutritious meal!"
-  puts "A plate of #{arg} with #{arg1} and #{arg2}."
+  puts "A plate of tofu with #{arg1} and #{arg2}."
 end
